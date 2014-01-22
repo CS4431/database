a2cd66af54ac06816192e7cedf5a8c9df53ef796
Sean Humeniuk <seanhumeniuk@gmail.com>
2014-01-21 21:33:26 -0500

    Add basic sqlite3 connection

--- src/routes.rb
+++ src/routes.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require 'sqlite3'
 
 class Routes < Sinatra::Base
   # Make server publicly accessible
@@ -34,6 +35,17 @@ class Routes < Sinatra::Base
     halt 400, "Bad extension" unless @extensions.include? ext
 
     "ISBN: #{isbn}, Extension: #{ext}"
+  end
+
+  get '/api/book/:id' do |id|
+    db = SQLite3::Database.open("db/books.sqlite")
+    query = "SELECT title, isbn FROM books WHERE _id=" + id.to_s
+    row = db.get_first_row(query)
+    if row == nil
+      return "No book found with id:" + id.to_s
+    end
+
+    return "Title: " + row[0] + "<br>ISBN: " + row[1].to_s
   end
 
   # Since we are subclassing Sinatra, we need to start Sinatra if being run directly
