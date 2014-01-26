38204a8d7cbb909bb1dbf0ae5d84341eac88ac2b
Sean Humeniuk <seanhumeniuk@gmail.com>
2014-01-25 21:11:05 -0500

    Add .xml serializer

--- src/serializer.rb
+++ src/serializer.rb
@@ -8,6 +8,32 @@ module Serializer
   # @param ext [String] format extension
   def Serializer.serialize(kind, data, ext)
     hash = { "kind" => kind, "data" => data }
-    JSON hash
+    case ext
+    when "json"
+      return JSON hash
+    when "xml"
+      hash = {kind => data}
+      return Serializer.to_xml hash
+    end
   end
+
+  def Serializer.to_xml(hash)
+    result = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+    result << Serializer.val_to_xml(hash).force_encoding("UTF-8")
+  end
+
+  def Serializer.val_to_xml(hash)
+    result = ""
+    hash.each do |k,v|
+      result << "<#{k}>"
+      if v.is_a? Hash
+        result << Serializer.val_to_xml(v)
+      else
+        result << v.to_s
+      end
+      result << "</#{k}>"
+    end
+    return result
+  end
+
 end
