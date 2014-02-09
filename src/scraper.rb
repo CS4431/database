29c7984a3e00f2f6cd5fab5da208feac5a89c212
Sean Humeniuk <seanhumeniuk@gmail.com>
2014-02-09 13:52:03 -0500

    Add book scraper

--- src/scraper.rb
+++ src/scraper.rb
@@ -108,4 +108,74 @@ module Scraper
     end
   end
 
+  # Parses all books in a course
+  #
+  # @param (see #get_all_timetables)
+  def Scraper.get_all_books(url)
+    page = Nokogiri::HTML(open(url))
+    book_titles = page.css('em')
+    text = page.to_s
+
+    img_url_base = "http://lakehead.bookware3000.ca/eSolution_config/partimg/large/"
+
+    editions = []
+    edition_hash = Hash.new
+    book_titles.each do |title|
+      # remove all text before book title
+      text = text.split(title)[1]
+      lines = text.split(/<br>|\n/)
+      looking_for = :isbn
+      lines.each do |line|
+
+        case looking_for
+        when :isbn
+          # look for isbn
+          if /ISBN: [0-9]*/.match(line)
+            edition_hash["title"] = title.text
+            isbn = line.gsub("ISBN: ", "")
+            isbn = isbn.gsub("&nbsp;", "").strip
+            edition_hash["isbn"] = isbn
+            edition_hash["image"] = img_url_base + isbn + ".jpg"
+            looking_for = :author
+          end
+        when :author
+          # look for author
+          if /Author: [.]*/.match(line)
+            author = line.gsub("Author: ", "")
+            author = author.gsub("&nbsp;", "").strip
+            edition_hash["author"] = author
+            looking_for = :publisher
+          end
+        when :publisher
+          # look for publisher
+          if /Publisher: [.]*/.match(line)
+            publisher = line.gsub("Publisher: ", "")
+            publisher = publisher.gsub("&nbsp;", "").strip
+            edition_hash["publisher"] = publisher
+            looking_for = :edition
+          end
+        when :edition
+          # look for edition
+          if /Edition: [.]*/.match(line)
+            edition = line.gsub("Edition: ", "")
+            edition = edition.gsub("&nbsp;", "").strip
+            edition_hash["edition"] = edition
+            looking_for = :cover
+          end
+        when :cover
+          # look for cover
+          if /Cover: [.]*/.match(line)
+            cover = line.gsub("Cover: ", "")
+            cover = cover.gsub("&nbsp;", "").strip
+            edition_hash["cover"] = cover
+
+            editions << edition_hash
+            edition_hash = Hash.new
+            break
+          end
+        end
+      end
+    end
+  end
+
 end
